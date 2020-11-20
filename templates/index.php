56f5167af507e57bc3633a773bc702168bfc4679
Richard Catterill <r_catterill@hotmail.com>
2020-11-20 15:56:18 +0000

    starting adding styling

--- templates/index.php
+++ templates/index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang='en'>
     <head>
-        <meta charset="utf-8"/>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
         <title>To Do App</title>
         <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+        <script src='https://kit.fontawesome.com/d224d393cb.js' crossorigin='anonymous'></script>
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' integrity='sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' crossorigin='anonymous'>
+        <script defer src='https://code.jquery.com/jquery-3.5.1.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>
+        <script defer src='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx' crossorigin='anonymous'></script>
         <style>
             body {
                 margin: 50px 0 0 0;
                 padding: 0;
                 width: 100%;
-                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+                font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
                 text-align: center;
                 color: #aaa;
                 font-size: 18px;
@@ -17,7 +22,7 @@
 
             h1 {
                 color: lightskyblue;
-                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+                font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
                 font-size: 50px;
                 margin-bottom: 0;
             }
@@ -52,32 +57,50 @@
                 <option <?php if(isset($_SESSION['categoryFilter']) && $_SESSION['categoryFilter'] == 'Work'){ echo 'selected';}else{ echo '';} ?>>Work</option>
                 <option <?php if(isset($_SESSION['categoryFilter']) && $_SESSION['categoryFilter'] == 'Personal'){ echo 'selected';}else{ echo '';} ?>>Personal</option>
             </select>
+            <div class='btn-group btn-group-toggle' data-toggle='buttons'>
+                <label class='btn btn-secondary active'>
+                    <input type='radio' name='categoryFilter' value='All' onChange='this.form.submit()' checked> All
+                </label>
+                <label class='btn btn-secondary'>
+                    <input type='radio' name='categoryFilter' value='None' onChange='this.form.submit()'> None
+                </label>
+                <label class='btn btn-secondary'>
+                    <input type='radio' name='categoryFilter' value='Personal' onChange='this.form.submit()'> Personal
+                </label>
+                <label class='btn btn-secondary'>
+                    <input type='radio' name='categoryFilter' value='Work' onChange='this.form.submit()'> Work
+                </label>
+            </div>
         </form>
         <table>
             <?php
-                echo
-                    '<tr>
+                if ($tasks) { ?>
+                    <tr>
                         <th>Task</th>
+                        <th>Category</th>
                         <th>Due</th>
-                    </tr>';
-                if ($tasks) {
-                    foreach ($tasks as $task) {
-                        echo
-                            '<tr class="'.$task['category'].'">'.
-                                '<td>'.
-                                    $task['task'].
-                                '</td>'.
-                                '<td>'.
-                                    $task['due'].
-                                '</td>'.
-                                '<td>'.
-                                    '<form action="/edit" method="get"><input name="'.$task['id'].'" type="submit" value="Edit"></form>'.
-                                '</td>'.
-                                '<td>'.
-                                    '<form action="/complete" method="post"><input name="'.$task['id'].'" type="submit" value="Mark complete"></form>'.
-                                '</td>'.
-                            '</tr>';
-                    }
+                    </tr>
+                    <?php foreach ($tasks as $task): ?>
+                        <tr>
+                            <td>
+                            <?= $task['task'] ?>
+                            </td>
+                            <td>
+                                <i class=' <?= $task['category'] == 'Work' ? 'fas fa-briefcase' : ($task['category'] == 'Personal' ? 'fas fa-walking' : '') ?> '/>
+                            </td>
+                            <td>
+                            <?= $task['due'] ?>
+                            </td>
+                            <td>
+                            <form action='/edit' method='get'>
+                                <input name=' <?= $task['id'] ?>' type='submit' value='Edit'>
+                            </form>
+                            </td>
+                            <td>
+                            <form action='/complete' method='post'><input name='<?= $task['id'] ?>' type='submit' value='Mark complete'></form>
+                            </td>
+                            </tr>
+                    <?php endforeach;
                 } else {
                     echo 'No active tasks';
                 }

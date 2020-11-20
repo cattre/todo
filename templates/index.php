894353d73f907ff313ab6f89c91ef14fd1864d2d
Richard Catterill <r_catterill@hotmail.com>
2020-11-20 16:00:17 +0000

    updating styling on index

--- templates/index.php
+++ templates/index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang='en'>
     <head>
-        <meta charset="utf-8"/>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
         <title>To Do App</title>
         <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+        <script src='https://kit.fontawesome.com/d224d393cb.js' crossorigin='anonymous'></script>
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' integrity='sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' crossorigin='anonymous'>
+        <script defer src='https://code.jquery.com/jquery-3.5.1.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>
+        <script defer src='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx' crossorigin='anonymous'></script>
         <style>
             body {
                 margin: 50px 0 0 0;
